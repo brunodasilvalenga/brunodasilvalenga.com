0539e5ddafe02cc269031271c4dae6da4f569928
Bruno da Silva Valenga <brunodasilvalenga@gmail.com>
2021-07-20 23:31:18 +1000

    rollback Image from next

--- next.config.js
+++ next.config.js
@@ -1,5 +1,3 @@
-const debug = process.env.NODE_ENV !== 'production'
-
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
@@ -40,5 +38,4 @@ module.exports = withBundleAnalyzer({
 
     return config
   },
-  assetPrefix: !debug ? 'https://brunodasilvalenga.github.io/' : '',
 })
