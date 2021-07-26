538b0f0bbf0ac4254ccbe8968ffe393ce791cf70
Bruno da Silva Valenga <brunodasilvalenga@gmail.com>
2021-07-27 00:25:45 +1000

    change font

--- pages/_document.js
+++ pages/_document.js
@@ -23,8 +23,9 @@ class MyDocument extends Document {
           <meta name="theme-color" content="#000000" />
           <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500&display=swap"
             rel="stylesheet"
           />
           <script
