4c0e85113ca32227c40fb5ef0a13e52a369f641d
Bruno da Silva Valenga <brunodasilvalenga@gmail.com>
2021-07-27 00:25:57 +1000

    change spaces

--- layouts/AuthorLayout.js
+++ layouts/AuthorLayout.js
@@ -22,7 +22,7 @@ export default function AuthorLayout({ children, frontMatter }) {
       <div className="divide-y">
         <div className="pt-6 pb-8 space-y-2 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            About
+            Hi there!
           </h1>
         </div>
         <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
@@ -30,7 +30,7 @@ export default function AuthorLayout({ children, frontMatter }) {
             <Image
               src={avatar}
               alt="avatar"
-              className="w-48 h-48 object-cover rounded-full"
+              className="w-48 h-48 object-cover rounded-md"
               width="192px"
               height="192px"
             />
--- layouts/ListLayout.js
+++ layouts/ListLayout.js
@@ -53,14 +53,14 @@ export default function ListLayout({ posts, title, initialDisplayPosts = [], pag
             const { slug, date, title, summary, tags } = frontMatter
             return (
               <li key={slug} className="py-4">
-                <article className="space-y-2 xl:grid xl:grid-cols-4 xl:space-y-0 xl:items-baseline">
+                <article className="space-y-2 xl:grid xl:grid-cols-6 xl:space-y-0 xl:items-baseline">
                   <dl>
                     <dt className="sr-only">Published on</dt>
                     <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
                       <time dateTime={date}>{formatDate(date)}</time>
                     </dd>
                   </dl>
-                  <div className="space-y-3 xl:col-span-3">
+                  <div className="space-y-3 xl:col-span-5">
                     <div>
                       <h3 className="text-2xl font-bold leading-8 tracking-tight">
                         <Link href={`/blog/${slug}`} className="text-gray-900 dark:text-gray-100">
